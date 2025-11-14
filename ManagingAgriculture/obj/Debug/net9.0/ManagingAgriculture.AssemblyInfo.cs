b9447c9daf13f6f4f01dde9a61c469da5f2d3ea6
vikihs2 <victorhstefanov@gmail.com>
2025-11-14 10:40:19 +0200

    Update assembly information and add new model files for Machinery, MarketplaceListing, Plant, Resource, Sensor, and SensorReading

--- ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
+++ ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d879178c494f25ab9d205ae455e8b695dfb4b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf463344a600a90ba98439e20d854468e09b820f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
