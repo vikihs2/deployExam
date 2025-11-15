d73bdc156076f72e73a19017d747738dd13178cf
vikihs2 <victorhstefanov@gmail.com>
2025-11-15 16:54:15 +0200

    fixed graphic

--- ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
+++ ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c57271ce437c239300daec2483479e8fe652e1a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c0e5ea267518319a28cc68ec19040613bba0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
