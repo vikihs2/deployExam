d8583b2e090e4d08c2cc97af2e65f810132bc733
vikihs2 <victorhstefanov@gmail.com>
2025-12-03 19:24:13 +0200

    changed photoes and some fixed issues

--- ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
+++ ManagingAgriculture/obj/Debug/net9.0/ManagingAgriculture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a94f56e9f48cae11cd8168c62d6a11ce7643c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d172c018c3719a419e7cb092933b667e3537c1e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagingAgriculture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
